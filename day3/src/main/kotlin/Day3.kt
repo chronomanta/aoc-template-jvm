dd880f40f9ea1a7bdabb54d4a953d91a7191c846
Michał Świeży <michal.swiezy@gmail.com>
2024-12-03 11:50:50 +0100

    Day 3 solution

--- day3/src/main/kotlin/Day3.kt
+++ day3/src/main/kotlin/Day3.kt
@@ -1,19 +1,43 @@
-fun main() {
+import java.util.regex.Pattern
+
+val pattern: Pattern = Pattern.compile("mul\\(\\d+,\\d+\\)")
+
+fun main(argv: Array<String>) {
     println(part1())
     println(part2())
 }
 
-fun part1(): Any {
-    return input
+fun String.sumMuls(): Int {
+    val matcher = pattern.matcher(this)
+    var sum = 0
+    while (matcher.find()) {
+        val (x, y) = matcher.group(0).drop(4).dropLast(1).split(",").map { it.toInt() }
+        sum += x * y
+    }
+    return sum
+}
 
+fun part1(): Any {
+    return input.sumMuls()
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val dontOnStart = input.startsWith("don't()")
+    val sections: List<String> = input.split("don't()")
+    var sum = if (dontOnStart) 0 else sections[0].sumMuls()
+    for (i in 1 until sections.size) {
+        val section = sections[i]
+        val index = section.indexOf("do()")
+        sum += if (index != -1)
+            section.drop(index + 4).sumMuls()
+        else
+            0
+    }
+    return sum
 }
 
 private val input: String by lazy {readInput()}
 
 private fun readInput(): String {
-    return object {}.javaClass.getResource("Day3.input").readText()
+    return object {}.javaClass.getResource("Day3.input")!!.readText()
 }
